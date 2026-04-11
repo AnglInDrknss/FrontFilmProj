7037845ac19f1c3d5300449b8b3cd696de8744c8
AnglInDrknss <artemijrybalkin@gmail.com>
2026-04-11 11:35:13 +0600

    Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,34 @@
+import {useState} from 'react';
+import { searchMovies } from '../services/api';
+
+
 export default function Home() {
-    return <div>Home</div>
+    const [query, setQuery] = useState('');
+    const [movies, setMovies] = useState([]);
+
+    const handleSearch = async () => {
+        searchMovies(query).then((data) => {
+            setMovies(data.titles);
+        });
+    };
+
+    return (
+        <div>
+            <input 
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)} 
+            />
+            <button onClick={handleSearch}>Search</button>
+            <div className="grid grid-cols-4 gap-2">
+                {movies.map((movie) => (
+                    <div className="p-2" key={movie.id}>
+                        <img src={movie?.primaryImage?.url} alt={movie.originalTitle} />
+                        <h3>{movie.originalTitle}</h3>
+                    </div>
+                ))}
+            </div>
+        </div>
+
+    );
 }
